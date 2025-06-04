ba450cc6665afee8fc6db562aec247555432a996
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-06-04 10:16:20 -0700

    Update Harry_Potter_cloak.py

--- Harry-Potter-Cloak/Harry_Potter_cloak.py
+++ Harry-Potter-Cloak/Harry_Potter_cloak.py
@@ -1,6 +1,4 @@
 
-    lower_s = cv2.getTrackbarPos("Lower Saturation", "Trackbars")
-    lower_v = cv2.getTrackbarPos("Lower Value", "Trackbars")
 
     lower_bound = np.array([lower_h, lower_s, lower_v])
     upper_bound = np.array([upper_h, upper_s, upper_v])
