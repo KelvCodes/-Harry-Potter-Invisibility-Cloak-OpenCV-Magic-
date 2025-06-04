eb6dc89bab5879bbb0849a20c0f014fb8892079d
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-06-04 10:16:10 -0700

    Update Harry_Potter_cloak.py

--- Harry-Potter-Cloak/Harry_Potter_cloak.py
+++ Harry-Potter-Cloak/Harry_Potter_cloak.py
@@ -1,5 +1,4 @@
 
-    lower_h = cv2.getTrackbarPos("Lower Hue", "Trackbars")
     lower_s = cv2.getTrackbarPos("Lower Saturation", "Trackbars")
     lower_v = cv2.getTrackbarPos("Lower Value", "Trackbars")
 
