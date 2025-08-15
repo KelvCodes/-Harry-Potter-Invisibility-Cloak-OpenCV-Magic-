0f958c929989d01a1f00b592cf85b3e5ffbde841
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-15 07:46:45 -0700

    Update Harry_Potter_cloak.py

--- Harry-Potter-Cloak/Harry_Potter_cloak.py
+++ Harry-Potter-Cloak/Harry_Potter_cloak.py
@@ -1,7 +1,4 @@
 
-
-
-# Trackbars for HSV thresholds
 cv2.createTrackbar("Lower Hue", "Trackbars", 68, 180, nothing)
 cv2.createTrackbar("Lower Saturation", "Trackbars", 55, 255, nothing)
 cv2.createTrackbar("Lower Value", "Trackbars", 54, 255, nothing)
