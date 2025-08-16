60117e625c57ebddafd262080d62037c3243c0ba
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-16 13:03:19 -0700

    Update Harry_Potter_cloak.py

--- Harry-Potter-Cloak/Harry_Potter_cloak.py
+++ Harry-Potter-Cloak/Harry_Potter_cloak.py
@@ -1,5 +1,4 @@
- trackbars for color range adjustment."""
-    cv2.createTrackbar("Lower Hue", window_name, 68, 180, trackbar_callback)
+ trackbars for color    cv2.createTrackbar("Lower Hue", window_name, 68, 180, trackbar_callback)
     cv2.createTrackbar("Lower Saturation", window_name, 55, 255, trackbar_callback)
     cv2.createTrackbar("Lower Value", window_name, 54, 255, trackbar_callback)
     cv2.createTrackbar("Upper Hue", window_name, 110, 180, trackbar_callback)
