8eacc2949972209c4702c9a66f5474df7855b8ae
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-16 13:04:17 -0700

    Update Harry_Potter_cloak.py

--- Harry-Potter-Cloak/Harry_Potter_cloak.py
+++ Harry-Potter-Cloak/Harry_Potter_cloak.py
@@ -1,5 +1,4 @@
-ateTrackbar("Upper Saturation", window_name, 255, 255, trackbar_callback)
-    cv2.createTrackbar("Upper Value", window_name, 255, 255, trackbar_callback)
+ateTrackbar("Upkbar("Upper Value", window_name, 255, 255, trackbar_callback)
 
 def get_hsv_bounds(window_name):
     """Retrieve HSV bounds from trackbars."""
