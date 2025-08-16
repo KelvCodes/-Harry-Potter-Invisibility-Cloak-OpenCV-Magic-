35e49fc36578847370c0f9801f256e10826b2e50
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-16 13:33:38 -0700

    Update Harry_Potter_cloak.py

--- Harry-Potter-Cloak/Harry_Potter_cloak.py
+++ Harry-Potter-Cloak/Harry_Potter_cloak.py
@@ -1,17 +1,1 @@
 
-        frame = cv2.flip(frame, 1)
-        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-       
-        cv2.imshow("Invisibility Effect", final_frame)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            print("👋 Closing effect...")
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-if __name__ == "__main__":
-    main()
-
