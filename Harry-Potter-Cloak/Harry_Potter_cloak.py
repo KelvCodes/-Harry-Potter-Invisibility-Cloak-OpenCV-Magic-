f6c9a6ef761ebf4f1cea14f1ef0b070e11feba79
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-18 15:24:40 -0800

    Update Harry_Potter_cloak.py

--- Harry-Potter-Cloak/Harry_Potter_cloak.py
+++ Harry-Potter-Cloak/Harry_Potter_cloak.py
@@ -1,4 +1,4 @@
-ackbar("LH", window_name, 68, 180, nothing)   # Lower Hue
+"LH", window_name, 68, 180, nothing)   # Lower Hue
     cv2.createTrackbar("LS", window_name, 55, 255, nothing)   # Lower Saturation
     cv2.createTrackbar("LV", window_name, 54, 255, nothing)   # Lower Value
     cv2.createTrackbar("UH", window_name, 110, 180, nothing)  # Upper Hue
