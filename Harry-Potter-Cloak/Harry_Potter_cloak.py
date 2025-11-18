b880851f04abb2e5d74cc045e09fa3a032d40cf3
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-17 23:43:53 -0800

    Update Harry_Potter_cloak.py

--- Harry-Potter-Cloak/Harry_Potter_cloak.py
+++ Harry-Potter-Cloak/Harry_Potter_cloak.py
@@ -1,5 +1,4 @@
-
-    """Create range trackbars for cloak detection."""
+ge trackbars for cloak detection."""
     cv2.createTrackbar("LH", window_name, 68, 180, nothing)   # Lower Hue
     cv2.createTrackbar("LS", window_name, 55, 255, nothing)   # Lower Saturation
     cv2.createTrackbar("LV", window_name, 54, 255, nothing)   # Lower Value
