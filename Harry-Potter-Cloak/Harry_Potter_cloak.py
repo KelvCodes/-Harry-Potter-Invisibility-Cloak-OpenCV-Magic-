b3ff5d010b764e768c4682035a557e605f4def8f
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-29 14:51:33 -0800

    Update Harry_Potter_cloak.py

--- Harry-Potter-Cloak/Harry_Potter_cloak.py
+++ Harry-Potter-Cloak/Harry_Potter_cloak.py
@@ -1,5 +1,4 @@
-Trackbar("UH", window_name, 110, 180, nothing)
-    cv2.createTrackbar("US", window_name, 255, 255, nothing)
+Trackbar("US", window_name, 255, 255, nothing)
     cv2.createTrackbar("UV", window_name, 255, 255, nothing)
 
 def get_trackbar_values(window_name):
