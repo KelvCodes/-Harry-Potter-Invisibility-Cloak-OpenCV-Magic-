71b6576a2ad87a197a6b45d5aca8bef53f8506f4
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-29 15:09:41 -0800

    Update Harry_Potter_cloak.py

--- Harry-Potter-Cloak/Harry_Potter_cloak.py
+++ Harry-Potter-Cloak/Harry_Potter_cloak.py
@@ -1,8 +1,6 @@
 
     ])
-
-    upper = np.array([
-        cv2.getTrackbarPos("UH", window_name),
+UH", window_name),
         cv2.getTrackbarPos("US", window_name),
         cv2.getTrackbarPos("UV", window_name),
     ])
