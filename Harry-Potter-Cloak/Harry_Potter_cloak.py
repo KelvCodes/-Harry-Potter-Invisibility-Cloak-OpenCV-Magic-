b0f05c766501c3484fb23a2b497566552f1bd005
Kelvin Agyare <onlykelvin06@gmail.com>
2025-12-02 09:13:19 -0800

    Update Harry_Potter_cloak.py

--- Harry-Potter-Cloak/Harry_Potter_cloak.py
+++ Harry-Potter-Cloak/Harry_Potter_cloak.py
@@ -1,4 +1,4 @@
-ounds."""
+
     lower = np.array([cv2.getTrackbarPos("LH", window_name),
                       cv2.getTrackbarPos("LS", window_name),
                       cv2.getTrackbarPos("LV", window_name)])
