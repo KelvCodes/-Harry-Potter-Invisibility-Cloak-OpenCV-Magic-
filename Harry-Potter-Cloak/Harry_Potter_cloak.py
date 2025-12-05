f30bb4658678a2d07a0c7037a6c46686419b4a20
Kelvin Agyare <onlykelvin06@gmail.com>
2025-12-05 10:20:22 -0800

    Update Harry_Potter_cloak.py

--- Harry-Potter-Cloak/Harry_Potter_cloak.py
+++ Harry-Potter-Cloak/Harry_Potter_cloak.py
@@ -1,4 +1,4 @@
-kbar_values(window_name):
+values(window_name):
     """Return HSV lower and upper bounds."""
     lower = np.array([cv2.getTrackbarPos("LH", window_name),
                       cv2.getTrackbarPos("LS", window_name),
