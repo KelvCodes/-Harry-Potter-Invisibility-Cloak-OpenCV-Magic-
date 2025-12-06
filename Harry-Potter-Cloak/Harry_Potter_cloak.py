7ba00599f4c2c5f8365afca3be000d2562d857fb
Kelvin Agyare <onlykelvin06@gmail.com>
2025-12-06 14:27:52 -0800

    Update Harry_Potter_cloak.py

--- Harry-Potter-Cloak/Harry_Potter_cloak.py
+++ Harry-Potter-Cloak/Harry_Potter_cloak.py
@@ -1,4 +1,4 @@
-trackbars(window_name):
+
     """Setup HSV sliders for manual color tuning."""
     cv2.createTrackbar("LH", window_name, 60, 180, nothing)
     cv2.createTrackbar("LS", window_name, 50, 255, nothing)
