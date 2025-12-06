14bc74e897a2b2364596b1a3757dd8660ea5fd73
Kelvin Agyare <onlykelvin06@gmail.com>
2025-12-06 14:28:15 -0800

    Update Harry_Potter_cloak.py

--- Harry-Potter-Cloak/Harry_Potter_cloak.py
+++ Harry-Potter-Cloak/Harry_Potter_cloak.py
@@ -1,5 +1,4 @@
-uning."""
-    cv2.createTrackbar("LH", window_name, 60, 180, nothing)
+ateTrackbar("LH", window_name, 60, 180, nothing)
     cv2.createTrackbar("LS", window_name, 50, 255, nothing)
     cv2.createTrackbar("LV", window_name, 40, 255, nothing)
     cv2.createTrackbar("UH", window_name, 120, 180, nothing)
