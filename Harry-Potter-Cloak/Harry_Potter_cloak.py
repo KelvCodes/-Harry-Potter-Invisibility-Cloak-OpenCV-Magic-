0ed514dc2f467afeedb5a3e2c0baff30c97a8b84
Kelvin Agyare <onlykelvin06@gmail.com>
2025-12-08 16:46:09 -0800

    Update Harry_Potter_cloak.py

--- Harry-Potter-Cloak/Harry_Potter_cloak.py
+++ Harry-Potter-Cloak/Harry_Potter_cloak.py
@@ -1,5 +1,4 @@
-
-def setup_trackbars(window_name):
+ars(window_name):
     """Create HSV adjustment trackbars."""
     cv2.createTrackbar("LH", window_name, 60, 180, nothing)
     cv2.createTrackbar("LS", window_name, 50, 255, nothing)
