147a12022ce62404f2d43834a1ed4dfcff5f6f55
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-01-01 11:00:55 -0800

    Update Harry_Potter_cloak.py

--- Harry-Potter-Cloak/Harry_Potter_cloak.py
+++ Harry-Potter-Cloak/Harry_Potter_cloak.py
@@ -1,4 +1,4 @@
-invisibility cloak implementation using OpenCV and NumPy.
+ cloak implementation using OpenCV and NumPy.
 The system captures a static background, detects a target color in HSV space,
 and replaces detected regions with the background to simulate invisibility.
 
