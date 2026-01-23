dad32d69d5d2b6aa6db72ced5b74373a5695f041
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-01-23 08:07:51 +0000

    Update Harry_Potter_cloak.py

--- Harry-Potter-Cloak/Harry_Potter_cloak.py
+++ Harry-Potter-Cloak/Harry_Potter_cloak.py
@@ -1,5 +1,4 @@
 
-        # Expand value range in dark conditions
         elif lighting_factor < 0.3:
             self.lower[2] = int(self.original_lower[2] * 0.6)
             self.upper[2] = int(min(255, self.original_upper[2] * 1.2))
