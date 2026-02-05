1b9974fe3f78699f150da56d3225026664162f4b
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-02-05 19:03:29 +0000

    Update Harry_Potter_cloak.py

--- Harry-Potter-Cloak/Harry_Potter_cloak.py
+++ Harry-Potter-Cloak/Harry_Potter_cloak.py
@@ -1,5 +1,4 @@
 
-    Dynamically adjust color detection
     use_multi_color: bool = True  # Detect multiple colors simultaneously
     max_colors: int = 3  # Maximum number of colors to detect
     
