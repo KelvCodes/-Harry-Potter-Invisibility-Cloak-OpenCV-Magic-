a351653e18b6117157379e778b97f5ef1aefa643
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-02-08 23:00:48 +0000

    Update Harry_Potter_cloak.py

--- Harry-Potter-Cloak/Harry_Potter_cloak.py
+++ Harry-Potter-Cloak/Harry_Potter_cloak.py
@@ -1,7 +1,4 @@
 
-    from 
-except ImportError:
-    SKLEARN_AVAILABLE = False
 
 # ============================================================================
 #                           LOGGING CONFIGURATION
